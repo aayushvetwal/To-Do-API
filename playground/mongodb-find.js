d6d918ce73942b3397702816f88e55cca68e5712
Aayush <aayushvetwal@gmail.com>
2017-12-03 18:43:51 -0500

    Initial Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,39 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find().toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  // db.collection('Todos').find({completed: false}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a230fe0ebfe8d1e4866f5e0')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err)
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log('Todos Count:', count);
+  }, (err) => {
+    console.log('Unable to fetch todos', err)
+  });
+
+  //db.close();
+});
