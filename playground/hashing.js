a43f64950c38826ebbcf660710212b3eeacbee16
Aayush <aayushvetwal@gmail.com>
2018-01-06 12:07:33 -0500

    Added POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,43 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+
+/*
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+console.log(`message: ${message}`);
+console.log(`hash: ${hash}`);
+
+//JSON Web Support(JWT) or Token based authentication Prototype/sample
+
+var data = {
+  id: 4
+};
+var token = {
+  data,
+  hash: SHA256(JSON.stringify(data) + 'somesecretsalt').toString()
+};
+
+//MAN IN THE MIDDLE
+// man in the middle doesn't have access to the salt
+token.data.id = 5;
+token.hash = SHA256(JSON.stringify((token.data))).toString();
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecretsalt').toString();
+
+if(resultHash === token.hash){
+  console.log('data was not manipulated');
+} else{
+  console.log('data was manipulated. don\'t trust.');
+}
+*/
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,58 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minLength: 1
-  }
+    minLength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
+
+UserSchema.methods.toJSON = function(){
+  var user = this;
+  var userObject = user.toObject(); //converts mongoose object to regular object where only properties available on document exists
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function(){
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, '123abc').toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+  //token is passed/returned as a success value to the next then call to be called in server.js file
+};
+//we used regular function instead of arrow function in the code above because arrow function doesn't bind 'this' keyword
+//Here, we need 'this' keyword to access individual document.
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
