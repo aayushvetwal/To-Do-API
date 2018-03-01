e047d8153ac45fee51f5b8396b115212e128faae
Aayush <aayushvetwal@gmail.com>
2018-03-01 08:19:17 -0500

    Updated config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,23 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if(env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json'); //automatically parses into json object
+    var envConfig = config[env];
+
+    //Object.keys(envConfig) returns the array ['PORT', 'MONGODB_URI']
+    Object.keys(envConfig).forEach((key) => {
+      process.env[key] = envConfig[key];
+    });
 }
+
+// if(env === 'development'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if(env === 'test'){
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
 
 //process.env.NODE_ENV is set by heroku when used on heroku (production environment). So, we don't
 //need to set it manually for heroku.
+  
